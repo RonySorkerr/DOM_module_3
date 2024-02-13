8fa0925501a2329e4e2bbcd64f0ab4d84c81cde4
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-13 11:54:54 +0600

    changed the rhombus funciton

--- script/rhombusArea.js
+++ script/rhombusArea.js
@@ -1,15 +1,15 @@
-function CalculateRhombusArea (){
-    const base = document.getInputById('d1');
-
-    const height = document.getInputById('d2')
-
+function CalculateRhombusArea(){
+    const base = getInputById('d1');
+    const height = getInputById('d2');
     const area = 0.5 * base * height;
 
-    setOutputById('rhombus_area_output', area);
+    setOutputById('rhombus_area_output', area.toFixed(4));
+    console.log('Area:', area);
+
 }
 
-function getInputById(inputField){
-    const inputField = document.getInputById(inputField);
+function getInputById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
     const inputValueText = inputField.value;
     const inputValue = parseFloat(inputValueText);
     return inputValue;
